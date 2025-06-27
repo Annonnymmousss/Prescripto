8f3dab2037c062ad590e995e51ca4b0fb70831a0
Annonnymmousss <jatin02012006@gmail.com>
2025-06-27 10:16:40 +0530

    feat: added the doctor dashboard panel

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -1,7 +1,7 @@
-
+import axios from 'axios'
 import { useState } from "react";
 import { createContext } from "react";
-
+import {toast} from 'react-toastify'
 
 
 export const DoctorContext = createContext()
@@ -9,10 +9,55 @@ export const DoctorContext = createContext()
 const DoctorContextProvider = (props) =>{
    const backendUrl = import.meta.env.VITE_BACKEND_URL
    const [dtoken , setdtoken] = useState(localStorage.getItem('dtoken')?localStorage.getItem('dtoken') : '')
+   const [appointments , setappointments] = useState([])
+
+   const getAppointments = async() => {
+    try {
+        const {data} = await axios.get(backendUrl + '/api/doctor/appointments' , {headers:{dtoken}})
+        if(data.success){
+            setappointments(data.appointments)
+        }else{
+            toast.error(data.message)
+        }
+    } catch (error) {
+        toast.error(error.message)
+        console.log(error)
+    }
+   }
+
+   const completeAppointment = async(appointmentId) => {
+    try {
+        const {data} = await axios.post(backendUrl + '/api/doctor/complete-appointment' , {appointmentId} , {headers:{dtoken}})
+        if(data.success){
+            toast.success(data.message)
+            getAppointments()
+        }else{
+            toast.error(data.message)
+        }
+    } catch (error) {
+        toast.error(error.message)
+        console.log(error)
+    }
+   }
+
+      const cancelAppointment = async(appointmentId) => {
+    try {
+        const {data} = await axios.post(backendUrl + '/api/doctor/cancel-appointment' , {appointmentId} , {headers:{dtoken}})
+        if(data.success){
+            toast.success(data.message)
+            getAppointments()
+        }else{
+            toast.error(data.message)
+        }
+    } catch (error) {
+        toast.error(error.message)
+        console.log(error)
+    }
+   }
 
 
     const value = {
-       dtoken , setdtoken , backendUrl
+       dtoken , setdtoken , backendUrl,appointments,setappointments,getAppointments,cancelAppointment,completeAppointment
     }
 
     return(
--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,6 +1,7 @@
 import doctorModel from "../models/doctorModel.js"
 import bcrypt from "bcrypt"
 import jwt from 'jsonwebtoken'
+import appointmentModel from "../models/appointmentModel.js"
 
 const changeAvailabiltiy = async(req,res) =>{
     try {
@@ -43,6 +44,57 @@ const loginDoctor = async(req,res) => {
         return res.json({success:false , message:error}) 
     }
 }
+
+const appointmentsDoctor = async(req,res) => {
+    try {
+        const docId = req.docId
+        const appointments = await appointmentModel.find({docId})
+        res.json({success:true,appointments})
+    } catch (error) {
+        console.log(error)
+        return res.json({success:false , message:error})
+    }
+}
+
+const appointmentComplete = async (req, res) => {
+    try {
+        const { appointmentId } = req.body;
+        const docId = req.docId;
+
+        const appointmentData = await appointmentModel.findById(appointmentId);
+
+        if (appointmentData && appointmentData.docId && appointmentData.docId.toString() === docId) {
+            await appointmentModel.findByIdAndUpdate(appointmentId, { isCompleted: true });
+            return res.json({ success: true, message: "Appointment Complete" });
+        } else {
+            return res.json({ success: false, message: "Mark Failed" });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.json({ success: false, message: error.message });
+    }
+};
+
+
+const appointmentCancelled = async (req, res) => {
+    try {
+        const { appointmentId } = req.body;
+        const docId = req.docId;
+
+        const appointmentData = await appointmentModel.findById(appointmentId);
+
+        if (appointmentData && appointmentData.docId && appointmentData.docId.toString() === docId) {
+            await appointmentModel.findByIdAndUpdate(appointmentId, { cancelled: true });
+            return res.json({ success: true, message: "Appointment Cancelled" });
+        } else {
+            return res.json({ success: false, message: "Cancellation Failed" });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.json({ success: false, message: error.message });
+    }
+};
+
 export {
-    changeAvailabiltiy,doctorList,loginDoctor
+    changeAvailabiltiy,doctorList,loginDoctor , appointmentsDoctor , appointmentCancelled , appointmentComplete
 }
